fac215a36d3083f95d63c3710e5cae2b497a7ecd
Igor Eulalio <igoreulalio@MacBook-Pro-de-Igor.local>
2021-05-04 15:32:39 -0300

    changing generate get presign url

--- download-object-csv/generate-get-presignurl.py
+++ download-object-csv/generate-get-presignurl.py
@@ -3,8 +3,10 @@ import boto3
 import os
 from botocore.exceptions import ClientError
 
-s3 = boto3.client("s3")
 bucket = os.environ.get("S3_BUCKET_NAME")
+region = os.environ.get("REGION")
+
+s3 = boto3.client("s3", region_name=region)
 
 
 def lambda_handler(event, context):
@@ -13,12 +15,16 @@ def lambda_handler(event, context):
 
     objet_from_s3 = getObject(object_key)
     
+    print(object_key)
+    print(bucket)
+    print(region)
+    
     return objet_from_s3
     
     
 def getObject(object_key):
     try:
-        return s3.get_object(Bucket=bucket, Key=object_key)
+        return s3.generate_presigned_url('get_object', Params={'Bucket': bucket, 'Key': object_key}, ExpiresIn = 15)
     except ClientError as ex:
         if ex.response['Error']['Code'] == 'NoSuchKey':
             return "Objeto com a key: " + object_key + " nao encontrado"
